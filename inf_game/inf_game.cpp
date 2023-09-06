f098bb2c1668b846013080067ea8bd22287e1335
moyasuri <moyasuri.hana@gmail.com>
2023-09-06 09:05:56 +0900

    0906-list-#01

--- inf_game/inf_game.cpp
+++ inf_game/inf_game.cpp
@@ -1,121 +1,107 @@
 ﻿
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 
+// 주제 : list
+// vector 동적배열, list 노드방식->tree 등 다른방식의 초석?
 
-//typedef int ab;
-//using test = int;
+// vector [       ] capacity 만큼 데이터를 모아놨다가 더커지면
+// vector [                        ] 더커진 capa를만들고 복사 붙여넣기를 해
+// 단일 / 이중 / 원형
+// list : 연결 리스트 !!!
 
-class MyIterator
+// list의 특징!
+// [1] ->link [2] -> [3] -> [4] -> [5]  // 단일
+// [1] <->link [2] <-> [3] <-> [4] <-> [5] // 이중
+// [1] <->link [2] <-> [3] <-> [4] <-> [5] <->...[1] // 원형
+// 연속된 공간에 배치되어야 된다는 강제성이 없어
+
+
+// Node2 [ int 4yte, next(data int 4byte , next))......] 이건 문법적으로 말이안돼
+class Node2
 {
+
 public:
+	// Node2 _next; // 봐 이건 통과 안되잖아
+	int _Data;
+};
+
+
+// Node[ int 4 byte _next(4 or 8byte)] -> Node [int 4byte _next(4 or 8byte)] -> ....
+
+class Node
+{
+
+public:
+
+	Node* _next; // 리스트 다음친구에게도 또 다른 노드가 있을테니 (포인터!)
+	Node* _prev; // 이중 리스트
+	// 처음에 좀 헷갈리면 void*포인터로 생각해보자
+	int _data;
+};
+
+
+
+
+
+int main() 
+{
+	// list (연결 리스트)
+	//	- 리스트의 동작원리
+	//	- 중간 삽입 삭제
+	//	- 처음 끝 삽입 삭제
+	//	- 임의접근
+
+	list<int> li;
+	for (int i = 0;i < 100; i ++ )
+	{
+		li.push_back(i);
+
+	}
+
+	li.push_front(10);
+	// vector는 push front를 지원하지 않았어, 굉장히 비효율적이야.
+
+	int size = li.size();
+	// li.capacity(); // 용량의 개념이 아니군 , 없어
 	
+	int fitst = li.front();
+	int last = li.back();
 
-};
+	// li[3] = 10; // 없음
 
-class MyVector
-{
-public:
-	typedef MyIterator iterator;
-	// c++11 using 문법을 쓰고 있다.
-};
-
-// 주제 : vector
-int main() {
+	list<int>::iterator itBegin; // iterator는 사용할 수 있어, 근데 동작방식은 vector랑 완전히 다를거야
+	itBegin= li.begin();  // 나눠 쓰기
 
 
-	// container 데이터를 저장하는 객체
-	// 
-	// 각 컨테이너마다 반복자라는걸 하나씩 들고 있다.
-	// 
-	// 반복자(iterator) -> 포인터와 유사한개념, 컨테이너의 원소(데이터)를 가리키고 , 다음 /이전 원소로 이동 가능
-
-	vector<int> v(10);
-	for (int i = 0;i < static_cast<unsigned int>(v.size());i++) // 혹은 vector<int>::size_type i=0;
+	list<int>::iterator itEnd = li.end(); // 선언+초기화
+	
+	for (list<int>::iterator it = li.begin();it != li.end(); it++)
 	{
-
-		v[i] = i;
+		cout << *it << endl;
 	}
 
-	vector<int>::iterator it;
-	// list, map.. 등등 container들이 각각 똑같은 이름으로 iterator로 되어있다.
-
-	MyVector::iterator it2; // 대강 이렇게 되어있을거라는 예쩨
-
-
-	int* ptr;
-	ptr = &v[0];
-	it = v.begin();
-
-	cout << (*it) << endl;
-	cout << (*ptr) << endl;
-
-	// 조사식에서 ptr의값을 받고, 그값을 메모리에서 확인해보면 0~9까지 들어가있는걸 확인할수있어.
-	// 조사식에서 it의 값을보면, ptr의 값은 당연히 있고 그외에 여러가지 myproxy 등등.. 추가적인값이 들어가 있다.
-	// 따지고보면 왜 굳이 포인터 쓰면 되지 iterator라는걸 쓰는 걸까?
-	// iterator는 포인터가아닌데 어떻게 포인터처럼 쓸 수 있는건가?
-	// operator overloading
-	// iterator자체는 클래스이지만 *를 붙여줌으로써 같은 역할을 하게끔 만든거야
-
-
-	// _Tptr _Ptr; // pointer to element in vector
-
-	// 포인터 ++ 연산은 주소값을 올려주는거니까
-	// 같은 이치로 it iterator도 주소값 올려주는거
+	li.insert(itBegin, 100);
+	
+	li.erase(li.begin()); // 어떤 위치에 있는 애를 삭제할때 쓰는데, 첫번쨋걸 삭제한경우
+	
+	li.pop_front(); // 첫번째껄 삭제할때는 이걸 더 많이 쓴다.
+	 
+	li.remove(10); // -> 10이랑 같은 값을 다 삭제해줘, 벡터는 애초에 중간 삭제가 너무 비효율적이야, 그리고 다돌아가면서 체크해줘야하니까
+	// 그래서 지원을 안해준듯.
 
 
 
-	vector<int>::iterator itBegin = v.begin();
-	vector<int>::iterator itEnd = v.end();
-
-	for (vector<int>::iterator it_ = v.begin();it_ != v.end();++it_) // 미세하게나마 ++it이 빨라
-	{
-		cout << (*it_) << endl;
-	}
-	// 포인터 증감후 바로 포인터를 뱉음
-
-	//_CONSTEXPR20 _Vector_const_iterator& operator++() noexcept {
-	//	++_Ptr;
-	//	return *this;
-	//}
-	// 
-	// 임시 변수에 포인터가 가리키는 값을 집어넣고, 포인터 증가후 임시변수를 내뱉음
-	//_CONSTEXPR20 _Vector_const_iterator operator++(int) noexcept {
-	//	_Vector_const_iterator _Tmp = *this;
-	//	++*this;
-	//	return _Tmp;
-	//}
-
-
-	int* ptrBegin = &v[0]; // = v.begin()._Ptr;
-
-	int* ptrEnd = ptrBegin + 10;  // = v.end()._Ptr;
-
-	for (int* ptr = ptrBegin; ptr != ptrEnd; ++ptr)
-	{
-		cout << (*ptr) << endl;
-	}
-
-
-	// const int*;
-	vector<int> ::const_iterator cit1 = v.cbegin();
-	// *cit1 = 100; 수정 불가
-
-
-	// iterator는 다른 컨테이너에도 있는 개념이며 v[i]같은 접근방법이 다른 컨테이너에서는 안될수 있으니
-	// iterator를 사용하는게 좀 더 통일성이 있다.
-
-	for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
-	{
-		cout << (*it) << endl;
-
-	}
-	// 역방향 사용할 일이 별로없음
 
 
 
+
+
+
+	return 0;
 
 }
